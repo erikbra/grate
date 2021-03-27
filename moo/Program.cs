9e955f54d1bf3338554f490fdb89941e70bbf6da
Erik A. Brandstadmoen <erik@brandstadmoen.net>
2021-03-27 22:08:25 +0100

    cleanups

--- moo/Program.cs
+++ moo/Program.cs
@@ -9,29 +9,19 @@ namespace moo
 {
     public static class Program
     {
-        
         private static readonly ServiceProvider ServiceProvider = BuildServiceProvider();
         private static bool _verbose;
 
         public static async Task<int> Main(string[] args)
         {
-
             var rootCommand = Create<MigrateCommand>();
             rootCommand.Add(Verbose());
             
-            //var rootCommand =new MigrateCommand()
-            //{
-                //Verbose(),
-            //};
-            
             rootCommand.Description = "The new moo - sql for the 20s";
-            //rootCommand.Handler = CommandHandler.Create<bool>(SetVerbose);
-            
             return await rootCommand.InvokeAsync(args);
         }
         
         private static void SetVerbose(bool verbose) => _verbose = verbose;
-        
         
         private static ServiceProvider BuildServiceProvider()
         {
