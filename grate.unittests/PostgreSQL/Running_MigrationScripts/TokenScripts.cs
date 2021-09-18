aa41595fcb571d71730308ec1f004943f3776490
Wokket <wokket@gmail.com>
2021-09-18 10:43:42 +1000

    tests: Added Token tests to Postgres and Sql as well

---
+++ grate.unittests/PostgreSQL/Running_MigrationScripts/TokenScripts.cs
@@ -0,0 +1,12 @@
+﻿using grate.unittests.TestInfrastructure;
+using NUnit.Framework;
+
+namespace grate.unittests.PostgreSQL.Running_MigrationScripts
+{
+    [TestFixture]
+    [Category("PostgreSQL")]
+    public class TokenScripts : Generic.Running_MigrationScripts.TokenScripts
+    {
+        protected override IGrateTestContext Context => GrateTestContext.PostgreSql;
+    }
+}
