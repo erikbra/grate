3e999ca1e506cc7ab124e7fa7b215b396cdebb89
Tim Thompson <github@wokket.com>
2021-09-24 04:35:27 +1000

    chore: refactored nunit logger into it's own file. (#43)

--- grate.unittests/TestInfrastructure/TestContextBase.cs
+++ grate.unittests/TestInfrastructure/TestContextBase.cs
@@ -1,71 +1,21 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using Microsoft.Extensions.Logging;
-using NUnit.Framework;
 
-namespace grate.unittests.TestInfrastructure
+namespace grate.unittests.TestInfrastructure;
+
+public abstract class TestContextBase : IDisposable
 {
-    public abstract class TestContextBase : IDisposable
+    public ILoggerFactory LogFactory = LoggerFactory.Create(builder =>
     {
-        public ILoggerFactory LogFactory = LoggerFactory.Create(builder =>
-        {
-            builder.AddProvider(new NUnitLoggerProvider())
-                   .SetMinimumLevel(LogLevel.Trace);
-        });
+        builder.AddProvider(new NUnitLoggerProvider())
+               .SetMinimumLevel(LogLevel.Trace); // TODO: Make this level configurable somehow...
+    });
 
-        public void Dispose()
-        {
-            LogFactory.Dispose();
-            GC.SuppressFinalize(this);
-        }
-    }
-
-    public class NUnitLogger : ILogger
+    public void Dispose()
     {
-        private readonly string _name;
-
-        public NUnitLogger(string name) => _name = name;
-
-        public IDisposable BeginScope<TState>(TState state) => default;
-
-        public bool IsEnabled(LogLevel logLevel) => true;
-
-        public void Log<TState>(
-            LogLevel logLevel,
-            EventId eventId,
-            TState state,
-            Exception exception,
-            Func<TState, Exception, string> formatter)
-        {
-            if (!IsEnabled(logLevel))
-            {
-                return;
-            }
-
-            TestContext.Progress.WriteLine($"     {_name} - {formatter(state, exception)}");
-
-        }
-    }
-
-    public class NUnitLoggerProvider : ILoggerProvider
-    {
-        public NUnitLoggerProvider() { }
-        //public NUnitLoggerProvider(EventHandler onCreateLogger)
-        //{
-        //    OnCreateLogger = onCreateLogger;
-        //}
-
-        public ConcurrentDictionary<string, NUnitLogger> Loggers { get; set; } = new ConcurrentDictionary<string, NUnitLogger>();
-
-        public ILogger CreateLogger(string categoryName)
-        {
-            NUnitLogger customLogger = Loggers.GetOrAdd(categoryName, new NUnitLogger(categoryName));
-            //OnCreateLogger?.Invoke(this, new NUnitLoggerProvider());
-            return customLogger;
-        }
-
-        public void Dispose() { GC.SuppressFinalize(this); }
-
-        public event EventHandler OnCreateLogger = delegate { };
+        LogFactory.Dispose();
+        GC.SuppressFinalize(this);
     }
 }
+
+
