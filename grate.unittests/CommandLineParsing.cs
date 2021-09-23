a80da88debab98e7face83be6645c1857fe55b9a
Wokket <wokket@gmail.com>
2021-09-24 06:23:13 +1000

    feat #31: First cut at --ignoreandwarnononetimescriptchanges

--- grate.unittests/CommandLineParsing.cs
+++ grate.unittests/CommandLineParsing.cs
@@ -186,6 +186,14 @@ namespace grate.unittests
             cfg?.WarnOnOneTimeScriptChanges.Should().Be(expected);
         }
 
+        [TestCase("", false)]
+        [TestCase("--warnandignoreononetimescriptchanges", true)]
+        public async Task WarnAndIgnoreOnOneTimeScriptChanges(string args, bool expected)
+        {
+            var cfg = await ParseGrateConfiguration(args);
+            cfg?.WarnAndIgnoreOnOneTimeScriptChanges.Should().Be(expected);
+        }
+
         [Test]
         public async Task WithoutTransaction_Default()
         {
