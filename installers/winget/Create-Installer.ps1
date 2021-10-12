b3e2a53316347dee56f6796f56c68020025f480e
Erik A. Brandstadmoen <erik@brandstadmoen.net>
2021-10-12 14:08:06 +0200

    Features/77 winget (#94)
    First attempt of winget stuff
    
    * Got first version accepted in winget (0.9.4)
    * Failed first, maybe failing due to unsigned msi
    * Add created winget manifests to git too (0.9.4, 0.9.5 and 0.9.6)
    * Added winget packages for 0.9.5 and 0.9.6

---
+++ installers/winget/Create-Installer.ps1
@@ -0,0 +1,45 @@
+#/bin/env pwsh
+
+param (
+    [string]$grateExe, # Not necessary, but try to keep a common interface for all Create-Installer.ps1 scripts
+    [Parameter(Mandatory=$true)] [string]$version 
+)
+
+$Root = Resolve-Path "$($PSScriptRoot)";
+
+$packageUrl="https://github.com/erikbra/grate/releases/download/$version/grate-$version.msi"
+
+# Download and extract winget
+$wingetExe="https://aka.ms/wingetcreate/latest/self-contained"
+$localExe="wingetcreate.exe"
+
+function Recreate {
+	param ($dirName)
+
+	$dir = Join-Path $Root $dirName;
+
+	If (Test-Path($dir)) {
+	   $null = Remove-Item -Recurse $dir
+	}
+	mkdir $dir;
+}
+
+$wingetDir = Recreate "winget"
+
+$client = New-Object System.Net.WebClient;
+$localFile = (Join-Path $wingetDir $localExe);
+
+try {
+    $client.DownloadFile($wingetExe, $localFile);
+} catch {
+   Write-Error "Error: " + $_
+   $error[0].Exception.ToString()
+}
+
+$winget = $localFile
+
+# Update the winget release
+
+& $winget update erikbra.grate -u $packageUrl -v $version -o $(Join-Path $Root grate)
+
+
