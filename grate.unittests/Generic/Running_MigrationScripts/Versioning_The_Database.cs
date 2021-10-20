f66a38f39625480329bf89733b4da34f906645ca
Erik A. Brandstadmoen <erik@brandstadmoen.net>
2021-10-20 11:44:50 +0200

    Bug #108: Version Id is not correctly set (#109)

---
+++ grate.unittests/Generic/Running_MigrationScripts/Versioning_The_Database.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using grate.Configuration;
+using grate.Migration;
+using grate.unittests.TestInfrastructure;
+using NUnit.Framework;
+
+namespace grate.unittests.Generic.Running_MigrationScripts
+{
+    [TestFixture]
+    public abstract class Versioning_The_Database : MigrationsScriptsBase
+    {
+        [Test]
+        public async Task Returns_The_New_Version_Id()
+        {
+            var db = TestConfig.RandomDatabase();
+
+            GrateMigrator? migrator;
+
+            var knownFolders = KnownFolders.In(CreateRandomTempDirectory());
+            CreateDummySql(knownFolders.Sprocs);
+            
+            await using (migrator = Context.GetMigrator(db, true, knownFolders))
+            {
+                await migrator.Migrate();
+
+                using (new AssertionScope())
+                {
+                    // Version again
+                    var version = await migrator.DbMigrator.VersionTheDatabase("1.2.3.4");
+                    version.Should().Be(2);
+                
+                    // And again
+                    version = await migrator.DbMigrator.VersionTheDatabase("1.2.3.4");
+                    version.Should().Be(3);
+                }
+            }
+        }
+    }
+}
