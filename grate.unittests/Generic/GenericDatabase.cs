c73c9ac3120f756a8bc088de328488b49d7b597d
Wokket <github@wokket.com>
2022-05-30 08:49:40 +1000

    fix #167: Ensure that a sql server database name that differs only by case doesn't error without good reason

--- grate.unittests/Generic/GenericDatabase.cs
+++ grate.unittests/Generic/GenericDatabase.cs
@@ -87,6 +87,24 @@ public abstract class GenericDatabase
         Assert.DoesNotThrowAsync(() => migrator.Migrate());
     }
 
+    [Test]
+    public async Task Does_not_needlessly_apply_case_sensitive_database_name_checks_Issue_167()
+    {
+        // There's a bug where if the database name specified by the user differs from the actual database only by case then
+        // Grate currently attempts to create the database again, only for it to fail on the DBMS (Sql Server bug only).
+
+        var db = "CASEDATABASE";
+        await CreateDatabase(db);
+
+        // Check that the database has been created
+        IEnumerable<string> databasesBeforeMigration = await GetDatabases();
+        databasesBeforeMigration.Should().Contain(db);
+
+        await using var migrator = GetMigrator(GetConfiguration(db.ToLower(), true)); // ToLower is important here, this reproduces the bug in #167
+        // There should be no errors running the migration
+        Assert.DoesNotThrowAsync(() => migrator.Migrate());
+    }
+
     protected virtual async Task CreateDatabase(string db)
     {
         using (new TransactionScope(TransactionScopeOption.Suppress, TransactionScopeAsyncFlowOption.Enabled))
