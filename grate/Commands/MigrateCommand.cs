59dbc6fa031aa1acab11d910b85ea7cd00f7bf3b
Jari Flederick <jari.flederick@infohos.be>
2022-06-01 00:11:14 +0200

    Add regions to the command options

--- grate/Commands/MigrateCommand.cs
+++ grate/Commands/MigrateCommand.cs
@@ -46,27 +46,25 @@ public sealed class MigrateCommand : RootCommand
             });
     }
 
-
-    private static Option Database() =>
-        new Option<string>(
-                new[] { "--database" },
-                "OBSOLETE: Please specify the connection string instead")
-            { IsRequired = false };
-
+    #region REQUIRED OPTIONS
     private static Option ConnectionString() =>
         new Option<string>(
                 new[] { "--connectionstring", "-c", "-cs", "--connstring" },
                 "You now provide an entire connection string. ServerName and Database are obsolete."
             )
             { IsRequired = true };
+    #endregion
 
+    #region CONNECTIONSTRING OPTIONS
     private static Option AdminConnectionString() =>
         new Option<string>(
                 new[] { "-csa", "-a", "--adminconnectionstring", "-acs", "--adminconnstring" },
                 "The connection string for connecting to master, if you want to create the database.  Defaults to the same as --connstring."
             )
             { IsRequired = false };
+    #endregion
 
+    #region DIRECTORY OPTIONS
     private static Option SqlFilesDirectory() =>
         new Option<DirectoryInfo>(
             new[] { "--sqlfilesdirectory", "-f", "--files" },
@@ -80,20 +78,17 @@ public sealed class MigrateCommand : RootCommand
             () => new DirectoryInfo(DefaultOutputPath),
             "This is where everything related to the migration is stored. This includes any backups, all items that ran, permission dumps, logs, etc."
         ).ExistingOnly();
+    #endregion
 
-    private static Option ServerName() =>
-        new Option<string>(
-            new[] { "--instance", "--server", "--servername", "-s" },
-            //() => DefaultServerName,
-            "OBSOLETE: Please specify the connection string instead."
-        );
-
+    #region SECURITY OPTIONS
     private static Option AccessToken() =>
         new Option<string>(
             new[] { "--accesstoken" },
             "Access token to be used for logging in to SQL Server / Azure SQL Database."
         );
+    #endregion
 
+    #region TIMEOUT OPTIONS
     private static Option CommandTimeout() =>
         new Option<int>(
             new[] { "--commandtimeout", "-ct" },
@@ -107,7 +102,9 @@ public sealed class MigrateCommand : RootCommand
             () => DefaultAdminCommandTimeout,
             "This is the timeout when administration commands are run (except for restore, which has its own)."
         );
+    #endregion
 
+    #region DATABASE OPTIONS
     private static Option DatabaseType() =>
         new Option<DatabaseType>(
             new[] { "--databasetype", "--dt", "--dbt" },
@@ -120,14 +117,6 @@ public sealed class MigrateCommand : RootCommand
             new[] { "--transaction", "--trx", "-t" },
             "Run the migration in a transaction"
         );
-
-    private static Option<GrateEnvironment?> Environment() =>
-        new (
-            aliases: new[] { "--env", "--environment" }, // we'll only support a single environment initially
-            parseArgument: ArgumentParsers.ParseEnvironment, // Needed in System.CommandLine beta3: https://github.com/dotnet/command-line-api/issues/1664
-            description: "Environment Name - This allows grate to be environment aware and only run scripts that are in a particular environment based on the name of the script.  'something.ENV.LOCAL.sql' would only be run if --env=LOCAL was set."
-        );
-
     private static Option<string> SchemaName() =>
         new(
             new[] { "--sc", "--schema", "--schemaname" },
@@ -135,29 +124,22 @@ public sealed class MigrateCommand : RootCommand
             "The schema to use for the migration tables"
         );
 
-    private static Option<bool> Silent() =>
-        new(
-            new[] { "--noninteractive", "-ni", "--ni", "--silent" },
-            "Silent - tells grate not to ask for any input when it runs."
-        );
-
-    private static Option<string> Version() =>
-        new(
-            new[] { "--version" }, // we can't use --version as it conflicts with the standard option
-            "Database Version - specify the version of the current migration directly on the command line."
-        );
-
     private static Option<bool> Drop() =>
         new(new[] { "--drop" },
             "Drop - This instructs grate to remove the target database.  Unlike RoundhousE grate will continue to run the migration scripts after the drop."
         );
+    #endregion
 
-    private static Option<bool> Tokens() =>
-        new(
-            new[] { "--disabletokenreplacement", "--disabletokens" },
-            "Tokens - This instructs grate to not perform token replacement ({{somename}}). Defaults to false."
+    #region ENVIRONMENT OPTIONS
+    private static Option<GrateEnvironment?> Environment() =>
+        new (
+            aliases: new[] { "--env", "--environment" },
+            parseArgument: ArgumentParsers.ParseEnvironment, // Needed in System.CommandLine beta3: https://github.com/dotnet/command-line-api/issues/1664
+            description: "Environment Name - This allows grate to be environment aware and only run scripts that are in a particular environment based on the name of the script.  'something.ENV.LOCAL.sql' would only be run if --env=LOCAL was set."
         );
+    #endregion
 
+    #region WARNING OPTIONS
     private static Option<bool> WarnAndRunOnScriptChange() =>
         new(
             new[] { "-w", "--warnononetimescriptchanges" },
@@ -169,13 +151,23 @@ public sealed class MigrateCommand : RootCommand
             new[] { "--warnandignoreononetimescriptchanges" },
             "WarnAndIgnoreOnOneTimeScriptChanges - Instructs grate to ignore and update the hash of changed one time scripts (DDL/DML in Up folder) that have previously been run against the database instead of failing. A warning is logged for each one time scripts that is rerun. Defaults to false."
         );
+    #endregion
+
+    #region TOKEN OPTIONS
+    private static Option<bool> Tokens() =>
+    new(
+        new[] { "--disabletokenreplacement", "--disabletokens" },
+        "Tokens - This instructs grate to not perform token replacement ({{somename}}). Defaults to false."
+    );
 
     private static Option<IEnumerable<string>> UserTokens() =>
         new(
             new[] { "--ut", "--usertokens" },
             "User Tokens - Allows grate to perform token replacement on custom tokens ({{my_token}}). Set as a key=value pair, eg '--ut=my_token=myvalue'. Can be specified multiple times."
         );
+    #endregion
 
+    #region SCRIPT OPTIONS
     private static Option<bool> DoNotStoreScriptText() =>
         new(
             new[] { "--donotstorescriptsruntext" },
@@ -187,7 +179,9 @@ public sealed class MigrateCommand : RootCommand
             new[] { "--runallanytimescripts", "--forceanytimescripts" },
             "RunAllAnyTimeScripts - This instructs grate to run any time scripts every time it is run even if they haven't changed. Defaults to false."
         );
+    #endregion
 
+    #region MISC OPTIONS
     private static Option<bool> Baseline() =>
         new(
             new[] { "--baseline" },
@@ -205,4 +199,32 @@ public sealed class MigrateCommand : RootCommand
             new[] { "--restore" },
             " Restore - This instructs grate where to get the backed up database file. Defaults to NULL."
         );
+
+    private static Option<bool> Silent() =>
+    new(
+        new[] { "--noninteractive", "-ni", "--ni", "--silent" },
+        "Silent - tells grate not to ask for any input when it runs."
+    );
+
+    private static Option<string> Version() =>
+        new(
+            new[] { "--version" }, // we can't use --version as it conflicts with the standard option
+            "Database Version - specify the version of the current migration directly on the command line."
+        );
+    #endregion
+
+    #region OBSOLETE OPTIONS
+    private static Option Database() =>
+        new Option<string>(
+                new[] { "--database" },
+                "OBSOLETE: Please specify the connection string instead")
+        { IsRequired = false };
+
+    private static Option ServerName() =>
+        new Option<string>(
+            new[] { "--instance", "--server", "--servername", "-s" },
+            //() => DefaultServerName,
+            "OBSOLETE: Please specify the connection string instead."
+        );
+    #endregion
 }
