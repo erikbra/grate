221ac136511ab3d2c97fe5ef38b812c33dadd427
Javier Najera <j.najera@accenture.com>
2022-07-23 01:20:59 -0500

    Adding a new test environment for MariaDB with case insensitive

--- grate.unittests/TestContext.cs
+++ grate.unittests/TestContext.cs
@@ -8,6 +8,8 @@ public static class GrateTestContext
     internal static readonly OracleGrateTestContext Oracle = new();
     internal static readonly PostgreSqlGrateTestContext PostgreSql = new();
     // ReSharper disable once InconsistentNaming
-    internal static readonly MariaDbGrateTestContext MariaDB = new();
+    internal static readonly MariaDbGrateTestContext MariaDB = new(true);
+    // ReSharper disable once InconsistentNaming
+    internal static readonly MariaDbGrateTestContext MariaDBCaseInsensitive = new(false);
     internal static readonly SqliteGrateTestContext Sqlite = new();
 }
