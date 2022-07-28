99663f3a87be5f3c74d1dbc6780307e77d42900d
Erik A. Brandstadmoen <erik@brandstadmoen@net>
2022-07-28 10:27:15 +0200

    Docker image per platform

--- grate.unittests/TestInfrastructure/TestContextBase.cs
+++ grate.unittests/TestInfrastructure/TestContextBase.cs
@@ -1,6 +1,16 @@
-﻿namespace grate.unittests.TestInfrastructure;
+﻿using System;
+using System.Runtime.InteropServices;
+using static System.Runtime.InteropServices.Architecture;
+
+namespace grate.unittests.TestInfrastructure;
 
 public abstract class TestContextBase
 {
-  
+    protected static string DockerPlatform() => RuntimeInformation.ProcessArchitecture switch
+    {
+        Arm64 => "linux/arm64",
+        X64 => "linux/amd64",
+        var other => throw new PlatformNotSupportedException("Unsupported platform for running tests: " + other)
+    };
+
 }
