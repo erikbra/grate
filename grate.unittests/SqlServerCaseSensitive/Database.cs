234c16cb280122b09a894d71677e0342231aa801
Tim Thompson <github@wokket.com>
2022-10-13 16:02:52 +1000

    fix: Added tests using case sensitive Sql server, and fixed issues with INFORMATION_SCHEMA (#251)

---
+++ grate.unittests/SqlServerCaseSensitive/Database.cs
@@ -0,0 +1,13 @@
+﻿using grate.unittests.TestInfrastructure;
+using NUnit.Framework;
+
+namespace grate.unittests.SqlServerCaseSensitive
+{
+    [TestFixture]
+    [Category("SqlServerCaseSensitive")]
+    public class Database : Generic.GenericDatabase
+    {
+        protected override IGrateTestContext Context => GrateTestContext.SqlServerCaseSensitive;
+
+    }
+}
---
+++ grate.unittests/SqlServerCaseSensitive/Running_MigrationScripts/Anytime_scripts.cs
@@ -0,0 +1,13 @@
+﻿using grate.unittests.TestInfrastructure;
+using NUnit.Framework;
+
+namespace grate.unittests.SqlServerCaseSensitive.Running_MigrationScripts
+{
+    [TestFixture]
+    [Category("SqlServerCaseSensitive")]
+    // ReSharper disable once InconsistentNaming
+    public class Anytime_scripts : Generic.Running_MigrationScripts.Anytime_scripts
+    {
+        protected override IGrateTestContext Context => GrateTestContext.SqlServerCaseSensitive;
+    }
+}
