7ba1f52ece2d0d9223d2801c1efe21b01a5e2edd
Wokket <github@wokket.com>
2023-01-09 06:57:32 +1000

    fix #282: Respect the users console background colours

--- grate/Infrastructure/TextWriterExtensions.cs
+++ grate/Infrastructure/TextWriterExtensions.cs
@@ -7,30 +7,30 @@ internal static class TextWriterExtensions
 {
     private static bool? _supportsAnsiColors;
 
-    public static void WriteColoredMessage(this TextWriter textWriter, string message, GrateConsoleColor background, GrateConsoleColor foreground)
+    public static void WriteColoredMessage(this TextWriter textWriter, string message, GrateConsoleColor foreground)
     {
-        SetColorsIfEnabled(textWriter, background, foreground);
+        SetColorsIfEnabled(textWriter, foreground);
         textWriter.Write(message);
         ResetColorsIfEnabled(textWriter);
     }
-        
-    public static void WriteColoredMessageLine(this TextWriter textWriter, string? message, GrateConsoleColor background, GrateConsoleColor foreground)
+
+    public static void WriteColoredMessageLine(this TextWriter textWriter, string? message, GrateConsoleColor foreground)
     {
-        SetColorsIfEnabled(textWriter, background, foreground);
+        SetColorsIfEnabled(textWriter, foreground);
         textWriter.WriteLine(message);
         ResetColorsIfEnabled(textWriter);
     }
-        
-            
-    private static void SetColorsIfEnabled(TextWriter textWriter, GrateConsoleColor background, GrateConsoleColor foreground)
+
+
+    private static void SetColorsIfEnabled(TextWriter textWriter, GrateConsoleColor foreground)
     {
         if (!DisableAnsiColors)
         {
-            SetColors(textWriter, background.AnsiColorCode, foreground.AnsiColorCode);
+            SetColors(textWriter, foreground.AnsiColorCode);
         }
     }
 
-        
+
     private static void ResetColorsIfEnabled(TextWriter textWriter)
     {
         if (!DisableAnsiColors)
@@ -38,32 +38,30 @@ internal static class TextWriterExtensions
             ResetColors(textWriter);
         }
     }
-        
+
     private static void ResetColors(TextWriter textWriter)
     {
         textWriter.Write(GrateConsoleColor.Foreground.Default.AnsiColorCode); // reset to default foreground color
-        textWriter.Write(GrateConsoleColor.Background.Default.AnsiColorCode); // reset to the background color
     }
 
-    private static void SetColors(TextWriter textWriter, string backgroundColorAnsiCode, string foregroundColorAnsiCode)
+    private static void SetColors(TextWriter textWriter, string foregroundColorAnsiCode)
     {
-        textWriter.Write(backgroundColorAnsiCode);
         textWriter.Write(foregroundColorAnsiCode);
     }
 
     private static bool DisableAnsiColors => !SupportsAnsiColors || Console.IsOutputRedirected;
-        
+
     private static bool SupportsAnsiColors => _supportsAnsiColors ??= GetSupportsAnsiColors();
 
     private static bool GetSupportsAnsiColors()
     {
         try
-            // Calling Console.GetCursorPosition() sometimes fails if the console has not been written to yet
+        // Calling Console.GetCursorPosition() sometimes fails if the console has not been written to yet
         {
             lock (Console.Out)
             {
                 var (oldPosition, _) = Console.GetCursorPosition();
-                SetColors(Console.Out, GrateConsoleColor.Background.Gray.AnsiColorCode, GrateConsoleColor.Foreground.Blue.AnsiColorCode);
+                SetColors(Console.Out, GrateConsoleColor.Foreground.Blue.AnsiColorCode);
                 var (currentPosition, yPos) = Console.GetCursorPosition();
 
                 ResetColors(Console.Out);
