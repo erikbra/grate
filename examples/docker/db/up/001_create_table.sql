cabc4b8abf0cb7dfd1fcc9e002b7eadea98420b8
Thanh Tran <hoangthanh28@gmail.com>
2023-12-26 10:10:50 +0700

    - update the docker build and its example

---
+++ examples/docker/db/up/001_create_table.sql
@@ -0,0 +1,4 @@
+ CREATE TABLE grate_test (
+                            id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+                            name nvarchar(255) NOT NULL
+                        )
