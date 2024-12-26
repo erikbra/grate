c1632e23df961df83514a0ef3a9473c4db8fee9f
Thanh Tran <hoangthanh28@gmail.com>
2024-12-26 13:19:54 +0700

    chore(mariadb): revert the bootstrapping script.

--- src/grate.mariadb/Bootstrapping/Sql/Baseline/up/03_create_scripts_run_errors_table.sql
+++ src/grate.mariadb/Bootstrapping/Sql/Baseline/up/03_create_scripts_run_errors_table.sql
@@ -1,18 +1,13 @@
-IF NOT EXISTS (SELECT 1 
-                  FROM INFORMATION_SCHEMA.TABLES
-                  WHERE table_schema = DATABASE() AND `table_name` = '{{SchemaName}}_{{ScriptsRunErrorsTable}}'
-            ) THEN
-        CREATE TABLE {{SchemaName}}_{{ScriptsRunErrorsTable}}(
-            id bigint NOT NULL AUTO_INCREMENT,
-            repository_path varchar(255) NULL,
-            version varchar(50) NULL,
-            script_name varchar(255) NULL,
-            text_of_script text NULL,
-            erroneous_part_of_script text NULL,
-            error_message text NULL,
-            entry_date timestamp NULL,
-            modified_date timestamp NULL,
-            entered_by varchar(50) NULL,
-            CONSTRAINT PK_{{ScriptsRunErrorsTable}}_id PRIMARY KEY (id)
-        );
-END IF;
+CREATE TABLE {{SchemaName}}_{{ScriptsRunErrorsTable}}(
+    id bigint NOT NULL AUTO_INCREMENT,
+    repository_path varchar(255) NULL,
+    version varchar(50) NULL,
+    script_name varchar(255) NULL,
+    text_of_script text NULL,
+    erroneous_part_of_script text NULL,
+    error_message text NULL,
+    entry_date timestamp NULL,
+    modified_date timestamp NULL,
+    entered_by varchar(50) NULL,
+    CONSTRAINT PK_{{ScriptsRunErrorsTable}}_id PRIMARY KEY (id)
+)
