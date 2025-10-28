c08da10bb2abf82ed6955d5e972a9ab1865fd7c0
Thanh Tran <hoangthanh28@gmail.com>
2025-10-29 01:29:36 +0700

    fix(sql-server): Remove text and ntext when creating grate internal tables (#662)
    * fix(sql-server): Remove text and ntext when creating grate internal tables.
    
    * Better approach if we get the hash of script and bypass if needed.

--- src/grate.sqlserver/Bootstrapping/Sql/Baseline/up/03_create_scripts_run_errors_table.sql
+++ src/grate.sqlserver/Bootstrapping/Sql/Baseline/up/03_create_scripts_run_errors_table.sql
@@ -1,14 +1,15 @@
-CREATE TABLE {{SchemaName}}.{{ScriptsRunErrorsTable}}(
-    id bigint IDENTITY(1,1) NOT NULL,
-    repository_path nvarchar(255) NULL,
-    version nvarchar(50) NULL,
-    script_name nvarchar(255) NULL,
-    text_of_script text NULL,
-    erroneous_part_of_script text NULL,
-    error_message text NULL,
-    entry_date datetime NULL,
-    modified_date datetime NULL,
-    entered_by nvarchar(50) NULL,
-    CONSTRAINT PK_{{ScriptsRunErrorsTable}}_id PRIMARY KEY CLUSTERED (id)
-)
-
+-- backwards compatibility, if the table already exists, let update scripts fix text column type issue
+IF NOT EXISTS (SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'{{SchemaName}}.{{ScriptsRunErrorsTable}}') AND type in (N'U'))
+    CREATE TABLE {{SchemaName}}.{{ScriptsRunErrorsTable}}(
+        id bigint IDENTITY(1,1) NOT NULL,
+        repository_path nvarchar(255) NULL,
+        version nvarchar(50) NULL,
+        script_name nvarchar(255) NULL,
+        text_of_script nvarchar(max) NULL,
+        erroneous_part_of_script nvarchar(max) NULL,
+        error_message nvarchar(max) NULL,
+        entry_date datetime NULL,
+        modified_date datetime NULL,
+        entered_by nvarchar(50) NULL,
+        CONSTRAINT PK_{{ScriptsRunErrorsTable}}_id PRIMARY KEY CLUSTERED (id)
+    )
